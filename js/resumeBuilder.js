06b13bc3c1616ecbebf76b2521ca49bd96055c9b
Xavier zanuy <xzanuy@gmail.com>
2016-03-19 20:24:29 +0100

    no message

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,70 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/*$("#main").append(["Xavier "]);
+
+
+var awesomeThoughts= "I am fucking awesome";
+
+var funThoughts = awesomeThoughts.replace("awesome","fun")
+
+console.log(funThoughts);
+$("#main").append([funThoughts]);
+
+var name="Xavier";
+var role="Front-End Developer"
+
+var formatedRole = HTMLheaderRole.replace("%data%", role)
+var formatedName = HTMLheaderName.replace("%data%",name)
+
+$("#header").prepend([formatedRole]);
+$("#header").prepend([formatedName]);
+$("#header").append([bio.name]);
+$("#header").append(work.position);
+$("#header").append(education.name);
+*/
+
+var bio ={
+	"name":"Xavier",
+	"role":"Front-End",
+	"contacts":{
+		"mobile":"602843983",
+		"email":"xzanuy@gmail.com",
+		"github":"xzanuy",
+		"location":"Barcelona"
+	},
+	"pictureURL":"https://media.licdn.com/mpr/mpr/shrink_100_100/AAEAAQAAAAAAAAQLAAAAJDdjZjc5NDQ3LTFhNjUtNDQ4NC05NzIwLTBiY2U2M2ZkODExZA.jpg",
+	"welcomeMessages":"Hello There!",
+	"skills":["HTML5", "CSS", "Javascript"]
+}
+
+
+
+var work = {
+	"position":"UX Developer",
+	"employer":"Talentier",
+	"city" :"Barcelona"
+
+}
+
+var projects={
+
+}
+
+
+
+var education = {
+	"schools": [{
+		"name": "Lafarga",
+		"city": "Sant Cugat",
+		"degree": "MBA",
+		"years": "4"
+
+	}, {
+		"name": "Upc",
+		"city": "Barcelona",
+		"degree": "MBA",
+		"years": "5"
+	}]
+
+}
+
+
+
